e58333624358b619f387cf1faf6149772a76d8ff
John <johnacreps@gmail.com>
2023-07-22 16:54:47 -0500

    Adding fonts and Images

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about-us">
+      <div className="about-content">
+        <h2>About Us - Legendary Software</h2>
+        <p>
+          At Legendary Software, we draw inspiration from the profound symbolism
+          of Shōbōji Temple's legendary 31-meter high Gingko Tree in
+          Higashimatsuyama, Japan. Just like this magnificent tree, our company
+          is deeply rooted in excellence and guided by unwavering principles. We
+          take pride in our commitment to delivering cutting-edge software
+          solutions that stand the test of time.
+        </p>
+        <p>
+          Growing with Innovation: Like the gingko tree that continues to grow
+          and adapt, we constantly evolve and embrace innovation. We believe in
+          staying at the forefront of technological advancements to craft
+          solutions that meet the ever-changing needs of our clients. Our agile
+          approach and forward-thinking mindset enable us to develop software
+          that resonates with the present and paves the way for the future.
+        </p>
+        {/* Add more paragraphs here to include the other sections */}
+      </div>
+    </section>
+  );
+};
+
+export default About;
